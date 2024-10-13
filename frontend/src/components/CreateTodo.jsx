416512c0b257145a5d2af14bcc36aa5c43b18768
Sanchit <sanchit04004@gmail.com>
2024-10-13 15:52:35 +0530

    Added CreateTodo.jsx and Todos.jsx file

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+
+export function CreateTodo(){
+    return <div>
+        <input style={{margin:10,padding:10}} type="text" placeholder="title"></input><br />
+        <input style={{margin:10,padding:10}} type="text" placeholder="description"></input><br />
+         
+         <button>Add a Todo</button>
+        
+    </div>
+}
